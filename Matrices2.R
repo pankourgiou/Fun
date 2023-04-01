e4692516b0f47f5cf11dde438580c013d99b0568
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-04-02 01:44:28 +0300

    Update and rename Prog.py to Matrices2.R

---
+++ Matrices2.R
@@ -0,0 +1,30 @@
+# Create a matrix
+thismatrix <- matrix(c(1,1,1,1,1,1), nrow = 3, ncol = 2)
+
+# Print the matrix
+thismatrix
+# Create a matrix
+thismatrix <- matrix(c(1,1,1,1,1,1), nrow = 3, ncol = 2)
+
+# Print the matrix
+thismatrix
+# Create a matrix
+thismatrix <- matrix(c(2222,2222,3333,4444,5555,6666), nrow = 3, ncol = 2)
+
+# Print the matrix
+thismatrix
+# Create a matrix
+thismatrix <- matrix(c(41,41,41,41,41,41), nrow = 3, ncol = 2)
+
+# Print the matrix
+thismatrix
+# Create a matrix
+thismatrix <- matrix(c(3000,3000,3000,3000,3000,3000), nrow = 3, ncol = 2)
+
+# Print the matrix
+thismatrix
+# Create a matrix
+thismatrix <- matrix(c(1111,2222,3333,4444,5555,6666), nrow = 3, ncol = 2)
+
+# Print the matrix
+thismatrix
--- Prog.py
+++
@@ -1,6 +0,0 @@
-from gtts import gTTS
-import os
-text = "To progress in a beautiful way"
-tts = gTTS(text)
-tts.save("hi.mp3")
-os.system("hi.mp3")
